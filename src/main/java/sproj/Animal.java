4f3d6db532263b68297f8247590516a585cac02d
alexander-hamme <ah2166@bard.edu>
2018-06-23 17:31:32 -0400

    finished basic Animal class

--- src/main/java/sproj/Animal.java
+++ src/main/java/sproj/Animal.java
@@ -3,6 +3,8 @@ package sproj;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 
+import java.lang.Math;
+
 public class Animal {
 
     final int BUFF_INDX = 3;        // buffer of data points to hold back when data is flushed to file, to run calculations on
@@ -34,19 +36,44 @@ public class Animal {
         this.x = x;
         this.y = y;
 
-        this.angle = calcKinematics(x, y, t);
+        calcKinematics(x, y, t);  // this.angle = ...
 
         this.dataPoints.add(new double[]{t, x, y, this.angle});
 
         this.linePoints.add(new int[]{x, y});
     }
 
-    private double calcKinematics(int x, int y, int t) {
+    private void calcKinematics(int x, int y, double t) {
         // calculate more than just angle
+
+        if (this.dataPoints.size() < BUFF_INDX) {
+            return;
+        }
+
+        ArrayList<double[]> prevPoints = (ArrayList<double[]>) this.dataPoints.subList(
+                this.dataPoints.size() - BUFF_INDX, this.dataPoints.size()
+        );
+
+        // time elapsed between current point and the first in list
+        double deltaTime = (t - prevPoints.get(0)[0]) / 1000.0;  // convert from milliseconds to seconds  so kinematics values are greater than zero
+
+        double dx = x - prevPoints.get(0)[0];
+        double dy = (-1) * y - prevPoints.get(0)[1];  // fix (computer graphics) y axis so increasing is up and decreasing is down
+
+        double vx = dx / deltaTime;
+        double vy = dy / deltaTime;
+
+        this.angle = 180 / Math.PI * Math.atan2(vy, vx);
+
+
     }
 
-    private double predictTrajectory() {
+    private double[] predictTrajectory() {
         // // TODO: 6/23/18        RNN Prediction here!!!
+
+
+
+        return null;
     }
 
 
