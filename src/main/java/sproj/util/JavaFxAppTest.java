e86838f2c06f2c69ee88eb78b7d018d803d32694
alexander-hamme <ah2166@bard.edu>
2018-09-13 14:17:02 -0400

    reorganizing code to integrate into JavaFX application

--- src/main/java/sproj/util/JavaFxAppTest.java
+++ src/main/java/sproj/util/JavaFxAppTest.java
@@ -2,6 +2,7 @@ package sproj.util;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -19,22 +20,53 @@ import org.bytedeco.javacv.*;
 import sproj.tracking.SinglePlateTracker;
 import sproj.tracking.Tracker;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 
 public class JavaFxAppTest extends Application {
+
+    int n_objs;
+    boolean drawShapes;
+    int[] crop;
+    CanvasFrame canvasFrame;
+    String videoPath;
+
+
+    private Image fxImgFrame;
+    private final ImageView iv = new ImageView(fxImgFrame);
+
+    final int WIDTH = 600;
+    final int HEIGHT = 400;
 
     private Java2DFrameConverter paintConverter = new Java2DFrameConverter();
 
     private Tracker trackerProgram;     // could be either SinglePlate or MultiPlate tracker class
 
 
-    public JavaFxAppTest(int n_objs, boolean drawShapes, int[] crop, CanvasFrame canvasFrame, String videoPath) throws IOException {
+    public JavaFxAppTest() throws IOException {
+        getValues();
         trackerProgram = new SinglePlateTracker(n_objs, drawShapes, crop, canvasFrame, videoPath);
     }
 
-    public void init() throws Exception {
+//    public void init() throws Exception {
+//    }
+
+    private void getValues() {
+        // input from user
+        n_objs = 5;
+        drawShapes = true;
+        // video file IMG_3086  -->  {60,210,500,500}
+        // video file IMG_3085  -->  550, 160, 500, 500
+        crop = new int[]{60,210,500,500};
+        canvasFrame = new CanvasFrame("Tracker");
+        videoPath = "src/main/resources/videos/IMG_3086.MOV";
+    }
+
+    private void timeStep() {
+
     }
 
     @Override
@@ -42,36 +74,44 @@ public class JavaFxAppTest extends Application {
         stage.setTitle("AnimatedImage Example");
 
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        root.getChildren().add(iv);
+
+
         stage.setScene(scene);
 
-        Canvas canvas = new Canvas(512, 512);
-        root.getChildren().add(canvas);
-
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+//        Canvas canvas = new Canvas(512, 512);
+//        root.getChildren().add(canvas);
+//
+//        GraphicsContext gc = canvas.getGraphicsContext2D();
 
 
 //        final long startNanoTime = System.nanoTime();
 
-        new AnimationTimer() {
+        AnimationTimer timer = new AnimationTimer() {
 
-            Image fxImgFrame = null;
+//            Image fxImgFrame = new Image(new File("src/main/resources/images/test_image.png").toURI().toString());
 
-
+            @Override
             public void handle(long currentNanoTime) {
 
+
+                timeStep();
                 try {
-                    fxImgFrame = convert(trackerProgram.timeStep());
-                } catch (IOException e) {
+                    fxImgFrame = convert(trackerProgram.timeStep());  // new Image(new File("src/main/resources/images/test_image.png").toURI().toString());  //
+                    iv.setImage(fxImgFrame);
+                } catch (Exception e) {
                     e.printStackTrace();
                     trackerProgram.tearDown();
                     stop();
                 }
 
-                gc.drawImage(fxImgFrame, 0, 0);
+//                gc.drawImage(fxImgFrame, 0, 0);
             }
-        }.start();
+        };
 
+        timer.start();
         stage.show();
     }
 
