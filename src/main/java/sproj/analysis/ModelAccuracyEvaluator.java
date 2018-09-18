a691feb1134d8f9eb0b8116e4d628e966f21fcd4
alexander-hamme <ah2166@bard.edu>
2018-09-18 17:10:35 -0400

    working on ModelAccuracyEvaluator

--- src/main/java/sproj/analysis/ModelAccuracyEvaluator.java
+++ src/main/java/sproj/analysis/ModelAccuracyEvaluator.java
@@ -1,21 +1,24 @@
 package sproj.analysis;
 
 
-import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.deeplearning4j.nn.layers.objdetect.DetectedObject;
-import sproj.tracking.Tracker;
-import sproj.util.DetectionsParser;
 import sproj.yolo_porting_attempts.YOLOModelContainer;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Class to test the accuracy of the underlying Yolo model
@@ -30,8 +33,8 @@ public class ModelAccuracyEvaluator {
     private OpenCVFrameConverter frameConverter = new OpenCVFrameConverter.ToMat();
 
 
-    private void initializeGrabber(String videoPath) throws FrameGrabber.Exception {
-        grabber = new FFmpegFrameGrabber(videoPath);
+    private void initializeGrabber(File videoFile) throws FrameGrabber.Exception {
+        grabber = new FFmpegFrameGrabber(videoFile);
         grabber.start();        // open video file
     }
 
@@ -39,15 +42,25 @@ public class ModelAccuracyEvaluator {
         yoloModelContainer = new YOLOModelContainer();
     }
 
-    public void evaluateModelOnVideo(String videoPath, int numbAnimals, Rect cropRectangle) throws IOException {
+    public List<Double> evaluateModelOnVideo(File videoFile, int numbAnimals, Rect cropRectangle) throws IOException {
 
-        initializeGrabber(videoPath);
-        evaluateOnVideo(numbAnimals, cropRectangle);
-
+        initializeGrabber(videoFile);
+        return evaluateOnVideo(numbAnimals, cropRectangle);
 
     }
 
 
+    /**
+     * Returns a list of detection accuracy evaluations, one for each frame of the video.
+     *
+     * For any given frame, if the model finds all animals correctly, the evaluation is 1.0, or 100%
+     *
+     * If the number of predictions
+     * @param numbAnimals
+     * @param cropRect
+     * @return
+     * @throws IOException
+     */
     private List<Double> evaluateOnVideo(double numbAnimals, Rect cropRect) throws IOException {
 
         List<DetectedObject> detectedObjects;
@@ -57,17 +70,103 @@ public class ModelAccuracyEvaluator {
         while ((frame = grabber.grabImage()) != null) {
 
             detectedObjects = yoloModelContainer.runInference(new Mat(frameConverter.convertToMat(frame), cropRect));
-            detectionAccuracies.add(detectedObjects.size() / numbAnimals);
-
+            double accuracy = detectedObjects.size() / numbAnimals;
+            detectionAccuracies.add(
+                    accuracy <= 1.0 ? accuracy : 1 - Math.abs(1 - accuracy)     // count each extra detection as one negative detection from the score
+            );
         }
-
+        return detectionAccuracies;
     }
-
 
     private void tearDown() {
         try {
             grabber.close();
         } catch (FrameGrabber.Exception ignored) {
         }
+    }
+
+    private List<String> readTextFiles(String textFile) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(textFile))) {
+            stream.forEach(lines::add);
+        }
+        return lines;
+    }
+
+
+    /**
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException, NumberFormatException {
+
+        System.out.print(Arrays.toString(args));
+
+        if (args.length < 2) {
+            System.out.println(
+                "Required: text file with list of video files to evaluate on.\n" +
+                "Syntax for this file is comma separated values, in this format:\n" +
+                "<full/path/to/video>,<number of animals in video>,<crop dimensions\n" +
+                "crop dimensions should be space separated integers, of the form: center_x center_y width height"
+            );
+            return;
+        }
+
+
+        ModelAccuracyEvaluator evaluator = new ModelAccuracyEvaluator();
+
+        List<String> textLines = evaluator.readTextFiles(args[1]);        // video files to evaluate on
+
+        List<List<Double>> detectionAccuracies = new ArrayList<>(textLines.size());
+
+
+        for (String line : textLines) {
+
+            String[] split = line.split(",");
+
+            if (split.length < 2) {continue;}
+
+            File videoFile;
+
+            int numbAnimals;
+            Rect cropRect;
+            List<Integer> cropDims = new ArrayList<>();
+
+            try {
+
+                videoFile = new File(split[0]);
+                assert videoFile.exists() && !videoFile.isDirectory();
+
+                numbAnimals = Integer.parseInt(split[1]);
+
+                // convert third string argument to 4 integers; the crop dimensions
+                Arrays.asList(split[2].split(" ")).forEach(s ->
+                        cropDims.add(Integer.valueOf(s)));
+
+                cropRect = new Rect(
+                        cropDims.get(0), cropDims.get(1), cropDims.get(2), cropDims.get(3)
+                );
+
+            } catch (AssertionError ignored) {
+                System.out.println(String.format(
+                        "Invalid video file path in line: '%s'", line
+                ));
+                continue;
+
+            } catch (NumberFormatException ignored) {
+                System.out.println(String.format(
+                        "Skipping incorrectly formatted line: '%s'", line
+                ));
+                continue;
+            }
+
+            detectionAccuracies.add(evaluator.evaluateModelOnVideo(videoFile, numbAnimals, cropRect));
+        }
+
+        detectionAccuracies.forEach(lst ->
+
+                System.out.println("Average accuracy: " + lst.stream().reduce(0.0, Double::sum) / lst.size())
+        );
     }
 }
