6a91c36cd7f6cbea3e3f5af28bbb778070b84dee
alexander-hamme <ah2166@bard.edu>
2018-10-04 14:49:35 -0400

    writing up webcam recorder program

--- src/main/java/sproj/util/WebcamRecorder.java
+++ src/main/java/sproj/util/WebcamRecorder.java
@@ -126,7 +126,7 @@ public class WebcamRecorder {
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         int currentNumberGroup = 1;     // group of 1 tadpole
         int numberOfTrialsToRecord = 20;
@@ -137,6 +137,8 @@ public class WebcamRecorder {
 
 
         for (int i=0; i<numberOfTrialsToRecord; i++) {
+
+            System.out.print("\r" + (i + 1) + " of " + numberOfTrialsToRecord + " videos recorded");
 
             String savePath = String.format("%s_%d", savePrefix, i+1);
             videoRecorder.recordVideo(lengthOfEachVideo, savePath);
@@ -221,84 +223,77 @@ public class WebcamRecorder {
 
 
         // Thread for audio capture, this could be in a nested private class if you prefer...
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // Pick a format...
-                // NOTE: It is better to enumerate the formats that the system supports,
-                // because getLine() can error out with any particular format...
-                // For us: 44.1 sample rate, 16 bits, stereo, signed, little endian
-                AudioFormat audioFormat = new AudioFormat(44100.0F, 16, 2, true, false);
+        new Thread(() -> {
+            // Pick a format...
+            // NOTE: It is better to enumerate the formats that the system supports,
+            // because getLine() can error out with any particular format...
+            // For us: 44.1 sample rate, 16 bits, stereo, signed, little endian
+            AudioFormat audioFormat = new AudioFormat(44100.0F, 16, 2, true, false);
 
-                // Get TargetDataLine with that format
-                Mixer.Info[] minfoSet = AudioSystem.getMixerInfo();
-                Mixer mixer = AudioSystem.getMixer(minfoSet[AUDIO_DEVICE_INDEX]);
-                DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+            // Get TargetDataLine with that format
+            Mixer.Info[] minfoSet = AudioSystem.getMixerInfo();
+            Mixer mixer = AudioSystem.getMixer(minfoSet[AUDIO_DEVICE_INDEX]);
+            DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
 
-                try
-                {
-                    // Open and start capturing audio
-                    // It's possible to have more control over the chosen audio device with this line:
-                    // TargetDataLine line = (TargetDataLine)mixer.getLine(dataLineInfo);
-                    TargetDataLine line = (TargetDataLine)AudioSystem.getLine(dataLineInfo);
-                    line.open(audioFormat);
-                    line.start();
+            try
+            {
+                // Open and start capturing audio
+                // It's possible to have more control over the chosen audio device with this line:
+                // TargetDataLine line = (TargetDataLine)mixer.getLine(dataLineInfo);
+                TargetDataLine line = (TargetDataLine)AudioSystem.getLine(dataLineInfo);
+                line.open(audioFormat);
+                line.start();
 
-                    int sampleRate = (int) audioFormat.getSampleRate();
-                    int numChannels = audioFormat.getChannels();
+                int sampleRate = (int) audioFormat.getSampleRate();
+                int numChannels = audioFormat.getChannels();
 
-                    // Let's initialize our audio buffer...
-                    int audioBufferSize = sampleRate * numChannels;
-                    byte[] audioBytes = new byte[audioBufferSize];
+                // Let's initialize our audio buffer...
+                int audioBufferSize = sampleRate * numChannels;
+                byte[] audioBytes = new byte[audioBufferSize];
 
-                    // Using a ScheduledThreadPoolExecutor vs a while loop with
-                    // a Thread.sleep will allow
-                    // us to get around some OS specific timing issues, and keep
-                    // to a more precise
-                    // clock as the fixed rate accounts for garbage collection
-                    // time, etc
-                    // a similar approach could be used for the webcam capture
-                    // as well, if you wish
-                    ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
-                    exec.scheduleAtFixedRate(new Runnable() {
-                        @Override
-                        public void run()
-                        {
-                            try
-                            {
-                                // Read from the line... non-blocking
-                                int nBytesRead = 0;
-                                while (nBytesRead == 0) {
-                                    nBytesRead = line.read(audioBytes, 0, line.available());
-                                }
-
-                                // Since we specified 16 bits in the AudioFormat,
-                                // we need to convert our read byte[] to short[]
-                                // (see source from FFmpegFrameRecorder.recordSamples for AV_SAMPLE_FMT_S16)
-                                // Let's initialize our short[] array
-                                int nSamplesRead = nBytesRead / 2;
-                                short[] samples = new short[nSamplesRead];
-
-                                // Let's wrap our short[] into a ShortBuffer and
-                                // pass it to recordSamples
-                                ByteBuffer.wrap(audioBytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(samples);
-                                ShortBuffer sBuff = ShortBuffer.wrap(samples, 0, nSamplesRead);
-
-                                // recorder is instance of
-                                // org.bytedeco.javacv.FFmpegFrameRecorder
-                                recorder.recordSamples(sampleRate, numChannels, sBuff);
-                            }
-                            catch (org.bytedeco.javacv.FrameRecorder.Exception e)
-                            {
-                                e.printStackTrace();
-                            }
+                // Using a ScheduledThreadPoolExecutor vs a while loop with
+                // a Thread.sleep will allow
+                // us to get around some OS specific timing issues, and keep
+                // to a more precise
+                // clock as the fixed rate accounts for garbage collection
+                // time, etc
+                // a similar approach could be used for the webcam capture
+                // as well, if you wish
+                ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+                exec.scheduleAtFixedRate(() -> {
+                    try
+                    {
+                        // Read from the line... non-blocking
+                        int nBytesRead = 0;
+                        while (nBytesRead == 0) {
+                            nBytesRead = line.read(audioBytes, 0, line.available());
                         }
-                    }, 0, (long) 1000 / FRAME_RATE, TimeUnit.MILLISECONDS);
-                }
-                catch (LineUnavailableException e1)
-                {
-                    e1.printStackTrace();
-                }
+
+                        // Since we specified 16 bits in the AudioFormat,
+                        // we need to convert our read byte[] to short[]
+                        // (see source from FFmpegFrameRecorder.recordSamples for AV_SAMPLE_FMT_S16)
+                        // Let's initialize our short[] array
+                        int nSamplesRead = nBytesRead / 2;
+                        short[] samples = new short[nSamplesRead];
+
+                        // Let's wrap our short[] into a ShortBuffer and
+                        // pass it to recordSamples
+                        ByteBuffer.wrap(audioBytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(samples);
+                        ShortBuffer sBuff = ShortBuffer.wrap(samples, 0, nSamplesRead);
+
+                        // recorder is instance of
+                        // org.bytedeco.javacv.FFmpegFrameRecorder
+                        recorder.recordSamples(sampleRate, numChannels, sBuff);
+                    }
+                    catch (FrameRecorder.Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }, 0, (long) 1000 / FRAME_RATE, TimeUnit.MILLISECONDS);
+            }
+            catch (LineUnavailableException e1)
+            {
+                e1.printStackTrace();
             }
         }).start();
 
