d9b5796f8443d8bcf3c4d91e67ca5cf4eb5c4836
alexander-hamme <ah2166@bard.edu>
2018-10-04 14:47:20 -0400

    writing up webcam recorder program

--- src/main/java/sproj/util/WebcamRecorder.java
+++ src/main/java/sproj/util/WebcamRecorder.java
@@ -6,6 +6,7 @@ import org.bytedeco.javacv.*;
 import javax.sound.sampled.*;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.ShortBuffer;
@@ -26,52 +27,47 @@ public class WebcamRecorder {
     private final static int FRAME_RATE = 30;
     private final static int GOP_LENGTH_IN_FRAMES = 60;
 
+    private final String VIDEO_FORMAT = "flv";
+
     private static long startTime = 0;
     private static long videoTS = 0;
 
     private FrameGrabber frameGrabber;
+    private FFmpegFrameRecorder recorder;
 
 
     private final int CAPTURE_WIDTH = 1280;
     private final int CAPTURE_HEIGHT = 720;
 
 
-    private void initializeFrameGrabber(String videoPath) throws FrameGrabber.Exception {
+    public WebcamRecorder(int cameraIndex) throws IOException {
 
-        if (new File(videoPath).exists()) {
-            frameGrabber = new FFmpegFrameGrabber(videoPath);
-            frameGrabber.start();    // open video file
-        } else {
-            int cameraIndex;
-            try{
-                cameraIndex = Integer.valueOf(videoPath);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Video path is neither a valid video file nor a camera index number", e);
-            }
-            frameGrabber = new OpenCVFrameGrabber(cameraIndex);
-            frameGrabber.start();
-        }
+        initializeFrameGrabber(cameraIndex);
+
     }
 
+    private void initializeFrameGrabber(int cameraIndex) throws FrameGrabber.Exception {
 
+//        if (new File(videoPath).exists()) {
+//            // if for
+//            frameGrabber = new FFmpegFrameGrabber(videoPath);
+//            frameGrabber.start();    // open video file
+//        } else {
 
+//        int cameraIndex;
+//        try{
+//            cameraIndex = Integer.valueOf(videoPath);
+//        } catch (NumberFormatException e) {
+//            throw new IllegalArgumentException("Video path is not a valid camera index number", e);
+//        }
+        frameGrabber = new OpenCVFrameGrabber(cameraIndex);
+        frameGrabber.setImageWidth(CAPTURE_WIDTH);
+        frameGrabber.setImageHeight(CAPTURE_HEIGHT);
+        frameGrabber.start();
 
-    public static void main(String[] args) throws Exception, org.bytedeco.javacv.FrameGrabber.Exception
-    {
-        int captureWidth = 1280;
-        int captureHeight = 720;
+    }
 
-        // The available FrameGrabber classes include OpenCVFrameGrabber (opencv_videoio),
-        // DC1394FrameGrabber, FlyCaptureFrameGrabber, OpenKinectFrameGrabber,
-        // PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
-        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(1);
-//        grabber.setImageWidth(captureWidth);
-//        grabber.setImageHeight(captureHeight);
-        grabber.start();
-
-
-
-        /*todo  write the video to file */
+    private void setUpRecorder(String videoPath) throws FrameRecorder.Exception {
         // org.bytedeco.javacv.FFmpegFrameRecorder.FFmpegFrameRecorder(String
         // filename, int imageWidth, int imageHeight, int audioChannels)
         // For each param, we're passing in...
@@ -80,8 +76,100 @@ public class WebcamRecorder {
         // imageWidth = width we specified for the grabber
         // imageHeight = height we specified for the grabber
         // audioChannels = 2, because we like stereo
-        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(
-                "/home/alex/Downloads/testVideo",
+        recorder = new FFmpegFrameRecorder(videoPath, CAPTURE_WIDTH, CAPTURE_HEIGHT, 2);
+        recorder.setInterleaved(true);
+
+        // decrease "startup" latency in FFMPEG (see:
+        // https://trac.ffmpeg.org/wiki/StreamingGuide)
+        recorder.setVideoOption("tune", "zerolatency");
+
+        // tradeoff between quality and encode speed
+        // possible values are ultrafast,superfast, veryfast, faster, fast,
+        // medium, slow, slower, veryslow
+        // ultrafast offers us the least amount of compression (lower encoder
+        // CPU) at the cost of a larger stream size
+        // at the other end, veryslow provides the best compression (high
+        // encoder CPU) while lowering the stream size
+        // (see: https://trac.ffmpeg.org/wiki/Encode/H.264)
+        recorder.setVideoOption("preset", "ultrafast");
+
+        // Constant Rate Factor (see: https://trac.ffmpeg.org/wiki/Encode/H.264)
+        recorder.setVideoOption("crf", "28");
+
+        // 2000 kb/s, reasonable "sane" area for 720
+        recorder.setVideoBitrate(2000000);
+
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+
+        recorder.setFormat(VIDEO_FORMAT);
+
+        // FPS (frames per second)
+        recorder.setFrameRate(FRAME_RATE);
+
+        // Key frame interval, in our case every 2 seconds -> 30 (fps) * 2 = 60
+        // (gop length)
+        recorder.setGopSize(GOP_LENGTH_IN_FRAMES);
+
+        // We don't want variable bitrate audio
+        recorder.setAudioOption("crf", "0");
+        // Highest quality
+        recorder.setAudioQuality(0);
+        // 192 Kbps
+        recorder.setAudioBitrate(192000);
+        recorder.setSampleRate(44100);
+        recorder.setAudioChannels(2);
+        recorder.setAudioCodec(avcodec.AV_CODEC_ID_AAC);
+
+        // Jack 'n coke... do it...
+        recorder.start();
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        int currentNumberGroup = 1;     // group of 1 tadpole
+        int numberOfTrialsToRecord = 20;
+        long lengthOfEachVideo = 30 * 1000;   // 30 seconds
+        String savePrefix = String.format("tadpoles%d", currentNumberGroup);
+
+        WebcamRecorder videoRecorder = new WebcamRecorder(1);
+
+
+        for (int i=0; i<numberOfTrialsToRecord; i++) {
+
+            String savePath = String.format("%s_%d", savePrefix, i+1);
+            videoRecorder.recordVideo(lengthOfEachVideo, savePath);
+        }
+
+
+
+
+    }
+    public void recordVideo(long duration, String savePath) throws IOException, InterruptedException {
+        int captureWidth = 1280;
+        int captureHeight = 720;
+
+        // The available FrameGrabber classes include OpenCVFrameGrabber (opencv_videoio),
+        // DC1394FrameGrabber, FlyCaptureFrameGrabber, OpenKinectFrameGrabber,
+        // PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
+
+//        frameGrabber = new OpenCVFrameGrabber(1);
+//        frameGrabber.setImageWidth(captureWidth);
+//        frameGrabber.setImageHeight(captureHeight);
+//        frameGrabber.start();
+
+        /*
+        // org.bytedeco.javacv.FFmpegFrameRecorder.FFmpegFrameRecorder(String
+        // filename, int imageWidth, int imageHeight, int audioChannels)
+        // For each param, we're passing in...
+        // filename = either a path to a local file we wish to create, or an
+        // RTMP url to an FMS / Wowza server
+        // imageWidth = width we specified for the frameGrabber
+        // imageHeight = height we specified for the frameGrabber
+        // audioChannels = 2, because we like stereo
+        recorder = new FFmpegFrameRecorder(
+                savePath,
                 captureWidth, captureHeight, 2);
         recorder.setInterleaved(true);
 
@@ -129,11 +217,13 @@ public class WebcamRecorder {
         // Jack 'n coke... do it...
         recorder.start();
 
+        */
+
+
         // Thread for audio capture, this could be in a nested private class if you prefer...
         new Thread(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 // Pick a format...
                 // NOTE: It is better to enumerate the formats that the system supports,
                 // because getLine() can error out with any particular format...
@@ -213,13 +303,17 @@ public class WebcamRecorder {
         }).start();
 
         // A really nice hardware accelerated component for our preview...
-        CanvasFrame cFrame = new CanvasFrame("Capture Preview", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+        CanvasFrame cFrame = new CanvasFrame("Capture Preview", CanvasFrame.getDefaultGamma() / frameGrabber.getGamma());
 
         Frame capturedFrame = null;
 
         // While we are capturing...
-        while ((capturedFrame = grabber.grab()) != null)
-        {
+        while ((capturedFrame = frameGrabber.grab()) != null) {
+
+            if (videoTS >= duration) {
+                break;
+            }
+
             if (cFrame.isVisible())
             {
                 // Show our frame in the preview
@@ -252,7 +346,7 @@ public class WebcamRecorder {
             // Send the frame to the org.bytedeco.javacv.FFmpegFrameRecorder
             recorder.record(capturedFrame);
 
-            KeyEvent keyPressed = cFrame.waitKey(100);
+            KeyEvent keyPressed = cFrame.waitKey(10);
             if (keyPressed != null) {
 
                 int keyChar = keyPressed.getKeyCode();
@@ -276,6 +370,6 @@ public class WebcamRecorder {
 
         cFrame.dispose();
         recorder.stop();
-        grabber.stop();
+        frameGrabber.stop();
     }
 }
